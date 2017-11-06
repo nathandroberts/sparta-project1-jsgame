39a3bf6d0efaa15ba568a86b8d73cedeca7f0282
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-11-06 12:21:56 +0000

    animation frame request added, paddle no longer functional

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-
-console.log(canvas);
-
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+  (function(){
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
+  })();
 
 var canvas = document.querySelector('canvas')
+console.log(canvas);
+//canvas width 800 height 500
 var content = canvas.getContext('2d');
 //ball variables
 var ballRadius = 12;
 var xBall = 400;
 var yBall= 450;
 var xBallSpeed = 2;
-var yBallSpeed = 1;
+var yBallSpeed = -1;
 //paddle variables
 var xPaddle = 300;
 var yPaddle = 460;
@@ -17,6 +21,7 @@ var paddleWidth = 200;
 var paddleHeight = 20;
 
 function makePaddle() {
+  content.clearRect((xPaddle - 5), (yPaddle- 5), (paddleWidth +10), (paddleHeight+ 10))
   content.beginPath();
   // xcoordinate y width height
   content.rect(xPaddle, yPaddle, paddleWidth, paddleHeight);
@@ -28,7 +33,7 @@ function makePaddle() {
 
 function makeBall() {
   // starting position x , y, width of erase box, height
-  content.clearRect(0,0, canvas.width, canvas.height)
+  content.clearRect((xBall - ballRadius - 3) , (yBall - ballRadius - 3) , (2.*ballRadius+ 10), (2.*ballRadius+ 10))
   content.beginPath();
   //ball x coordinate, y coordinate,radius start angle of circle, endAngle
   content.arc(xBall, yBall, ballRadius, 0, Math.PI * 2);
@@ -43,20 +48,36 @@ function makeBall() {
   if (yBall > canvas.height || yBall <0) {
   yBallSpeed = -yBallSpeed;
   }
+  requestAnimationFrame(makeBall);
 }
 function movePaddle() {
   canvas.addEventListener('mousemove', function (event) {
     var xMouse = event.clientX;     // Get the horizontal coordinate
     var yMouse = event.clientY;
-    console.log(yMouse);
+
     xPaddle = xMouse
     // 390 480 y limits
     if (yMouse > 390 && yMouse < 470) {
-
       yPaddle = yMouse
     }
+    collisionDetectionPaddle(yPaddle, yBall)
   })
+  requestAnimationFrame(movePaddle)
 }
+function collisionDetectionPaddle(yPaddleCoordinate, yBallCoordinate) {
+  for (var i = 0; i < (xPaddle + paddleWidth); i++) {
+
+    if (yPaddleCoordinate === yBallCoordinate && (xPaddle+ i) === xBall ){
+      yBallSpeed = -yBallSpeed
+      console.log('xPaddle' + ' '+ i);
+    }
+  }
+}
+
 movePaddle()
-setInterval(makePaddle, 5)
-setInterval(makeBall,10)
+// setInterval(makePaddle, 10)
+// setInterval(makeBall,10)
+makePaddle()
+makeBall()
+
+});
